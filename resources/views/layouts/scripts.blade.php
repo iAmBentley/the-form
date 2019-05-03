ae5a7a4023431c3d4c4d12196ffaa45d672f882e
iAmBentley <joey@moticorp.com>
2019-05-03 11:19:32 -0400

    Tweak gitignore + update jquery to latest cdn

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,5 +1,5 @@
 {{-- CORE JS --}}
-<script src="{{ asset('/js/jquery-3.3.1.min.js') }}"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 <script
     src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js"
     integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em"
